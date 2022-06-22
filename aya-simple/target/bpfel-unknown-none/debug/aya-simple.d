8693a09e17bfddf7232a6d98b5653024f33cbd72
tirex <tirexgta@gmail.com>
2022-06-22 11:00:03 +0200

    Update ebpf binary

---
+++ aya-simple/target/bpfel-unknown-none/debug/aya-simple.d
@@ -0,0 +1,1 @@
+/home/szymon/projects/ebpf-bug/aya-simple/aya-simple-ebpf/../target/bpfel-unknown-none/debug/aya-simple: /home/szymon/projects/ebpf-bug/aya-simple/aya-simple-common/src/lib.rs /home/szymon/projects/ebpf-bug/aya-simple/aya-simple-ebpf/src/main.rs
